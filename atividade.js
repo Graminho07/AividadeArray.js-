ac4f6ceb55113db8eb818bdc22efa40a96dc5f7a
Graminho07 <101339650+Graminho07@users.noreply.github.com>
2022-05-26 10:51:56 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,41 @@
+// //5 nomes
+// let nomes = ["Augusto", "Liara", "Bruno", "Alek", "Isaque"]
+
+// //8 Salários
+// let salario = ["R$4000", "R$5000", "R$9000", "R$8000", "R$3000", "R$1000", "R$10000","R$7000"]
+
+// //Booleanos 
+// let verdade = [true, false]
+
+//Produto e desconto
+// let precos = [35, 54, 49]
+//     console.log("Hamburger", precos[0])
+//     console.log("Hamburger com 5% de desconto", precos[0] - (precos[0] * 5)/100)
+//     console.log("Batata", precos[1])
+//     console.log("Batata com 5% de desconto", precos[1] - (precos[1] * 5)/100)
+//     console.log("Suco", precos[2])
+//     console.log("Suco com 5% de desconto", precos[2] - (precos[2] * 5)/100)
+
+// //Matriz de ordem 3
+// let matriz = [[4,6,8], [6, 7, 8], [2, 9, 3]]
+// for(let i in matriz){
+//     console.log("|", matriz[i][0], matriz[i][1], matriz[i][2], "|")
+// }
+// console.log("Diagonal Multiplicada")
+//     matriz[0][0] = 2
+//     matriz[1][1]= 2
+//     matriz[2][1] = 2
+// for(let i in matriz){
+//     console.log("|", matriz[i][0], matriz[i][1], matriz[i][2], "|")
+// }
+
+// Matriz de ordem 2 + determinante
+// let matriz = []
+// for(let i = 0; i < 4; i++){
+//     matriz.push(Math.floor(Math.random()10))
+// }
+// console.log("|", matriz[0], matriz[1],"|")
+// console.log("|", matriz[2], matriz[3],"|")
+
+// let det = (matriz[0] * matriz[3]) - (matriz[1] * matriz[2])
+// console.log("O determinante dessa matriz é", Math.abs(det))
